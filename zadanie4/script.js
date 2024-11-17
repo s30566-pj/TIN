2f6a96b6fafd166bd46894a8888a49ee5358e4af
s30566-pj <146527974+s30566-pj@users.noreply.github.com>
2024-11-17 12:45:58 +0100

    vars

--- zadanie4/script.js
+++ zadanie4/script.js
@@ -1,5 +1,8 @@
 function pythagorean(){
     let liczby = [a, b, c];
+    var a;
+    var b;
+    var c;
     liczby.sort((x, y) => x - y);
 
     return (liczby[0] ** 2 + liczby[1] ** 2 === liczby[2] ** 2);
